5fbf30f856806b6a7931a76b6969d56068d2017b
drew <droo@hey.com>
2021-02-23 21:20:19 -0800

    got custom manage.py command (connector) working

--- dashboard/app/management/commands/connector.py
+++ dashboard/app/management/commands/connector.py
@@ -1,7 +1,8 @@
-import os
-import json
+import os, io, json
 from django.core.management.base import BaseCommand, CommandError
 from app.models import Freedge, Shelter
+
+from pprint import pprint
 
 class Command(BaseCommand):
 
@@ -11,29 +12,27 @@ class Command(BaseCommand):
         ingestFolder = r'./core/static/ingest/'
         ingests = os.scandir(ingestFolder)
 
-        for file in ingests:
-            #with open(file.path) as f:
-            f = open(file.path, 'r')
-            contents = f.read()
-            data = json.loads(contents)
+        for f in ingests:
+            file = io.open(f.path, 'r', encoding='utf-8-sig')
+            data = json.load(file)
             if 'food' in f.name:
                 for entry in data:
                     print(entry)
                     record = Freedge(
-                                    name = entry['title'],
-                                    address = entry['StreetAddress'],
-                                    location = entry['StreetAddress'],
-                                    availability = 'y',
-                                    phone = entry['PhoneNumber'],
-                                    date_updated = ''
-                                )
+                        name = entry['Title'],
+                        address = entry['StreetAddress'],
+                        location = entry['StreetAddress'],
+                        availability = 'y',
+                        phone = entry['PhoneNumber'],
+                        date_updated = ''
+                    )
                     print(record)
 
             if 'shelter' in f.name:
                 for entry in data:
                     print(entry)
                     record = Shelter(
-                                    name = entry['title'],
+                                    name = entry['Title'],
                                     address = entry['StreetAddress'],
                                     location = entry['StreetAddress'],
                                     availability = 'y',
